10a1387b59af0bd474e26be124b1e1324ca1d97a
Felipe Mateus <felipe_mateus08@hotmail.com>
2022-03-16 15:20:18 -0300

    iniciando modulo de functions

---
+++ 1-estruturas-de-programacao/functions/functions.js
@@ -0,0 +1,2 @@
+// Para executar uma funcao -> nameFunction();
+// A function pode receber paramestros -> nameFunction(parametro1, parametro2);
---
+++ 1-estruturas-de-programacao/variables/variables.js
@@ -0,0 +1,22 @@
+// declarando variaveis
+// var -> metodo descontinuado para declaracao de variaveis em js
+// const -> valor que nunca muda
+// let -> declaracao atual de variaveis em js
+let orange = 2;
+const red = 3;
+var blue = 4;
+// shorthand na declaracao de variaveis
+let name1 = "Felipe",
+  name2 = "Mateus",
+  idade = 22;
+
+// utilizando variaveis com statments
+console.log(`Orange * Red = ${orange * red}`);
+console.log(`O valor de blue e: ${blue}`);
+console.log(`Meu nome e ${name1} ${name2}, e tenho ${idade} anos!`);
+
+// // Convencao no nome de variaveis:
+// variaveis nao podem comecar com numeros:
+// let 2red = "teste";
+// mas podem terminar com numeros:
+// let red2 = "teste2";
